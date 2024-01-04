881b3fb81dab7273464e4ad2507e594a1a51b5ef
Marion-Ankunda <scentsationaal@gmail.com>
2024-01-04 16:31:39 +0300

    cors test

--- essential funcs/essentials/middleware.js
+++ essential funcs/essentials/middleware.js
@@ -1,6 +1,8 @@
 const cors = require('cors');
 var allowlist = ['http://localhost:5173', 'scentsational-git-main-marions-projects-7c99b152.vercel.app', 'https://scentsational.vercel.app/']
 async  function corsOptionsDelegate (req,res,next) {
+    console.log(req.header('origin'));
+    console.log(req.headers.origin);
     var corsOptions;
     if (allowlist.indexOf(req.header('origin')) !== -1) {
         corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
