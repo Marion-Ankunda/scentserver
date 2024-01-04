5f0110aedc31e1bead7a41649f9a31032b1ca0eb
Marion-Ankunda <scentsationaal@gmail.com>
2024-01-04 16:03:13 +0300

    no cors

--- essential funcs/essentials/middleware.js
+++ essential funcs/essentials/middleware.js
@@ -5,7 +5,7 @@ let configs = {
     "origin": 'https://tame-pear-duck-sock.cyclic.app',
     "optionsSuccessStatus": 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-var allowlist = ['scentsational-b1ue6858h-marions-projects-7c99b152.vercel.app','http://localhost:5173','scentsational-git-main-marions-projects-7c99b152.vercel.app','https://scentsational.vercel.app/']
+var allowlist = ['http://localhost:5173','scentsational-git-main-marions-projects-7c99b152.vercel.app','https://scentsational.vercel.app/']
 var corsOptionsDelegate = function (req,res,next) {
     var corsOptions;
     console.log(req.header('origin'));
