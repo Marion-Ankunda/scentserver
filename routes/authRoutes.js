bc01115ab4ec724001be64e7b22dec9db13a1398
Marion-Ankunda <scentsationaal@gmail.com>
2024-01-07 20:09:50 +0300

    email

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,25 +6,25 @@ const router = require('express').Router()
 
 router.get('/scentsapi/signup', (req, res) => {
     corsOptionsDelegate(req, res, () => {
-    try {
-        signUpfunc(req.query.email, req.query.password).then((e) => {
-            res.send(e)
-        })
-    } catch (error) {
-        res.send('error')
-    }})
+        try {
+            signUpfunc(req.query.email, req.query.password).then((e) => {
+                res.send(e)
+            })
+        } catch (error) {
+            res.send('error')
+        }
+    })
 })
 router.get('/scentsapi/keys/auth', (req, res) => {
-    corsOptionsDelegate(req, res, () => {
-            try {
-                res.send(firebaseConfig)
-            } catch (error) {
-                console.log(error);
-                res.send('error')
-            }
-    })
+    console.log(12434);
+    console.log(firebaseConfig);
+    try {
+        res.send(firebaseConfig)
+    } catch (error) {
+        console.log(error);
+        res.send('error')
+    }
+})
 
-}
-)
 
 module.exports = router;
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,9 +3,7 @@ const { sendEmail } = require('../essential funcs/generateEmail/sendMail');
 const router = require('express').Router()
 
 router.get('/scentsational/sendEmail/', (req, res) => {
-  corsOptionsDelegate(req, res, () => {  
     sendEmail(req.query)
-  })
 })
 
 module.exports = router;
