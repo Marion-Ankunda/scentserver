37178c7d240afe78b6a13ac8d48acdc6dc026686
Marion-Ankunda <scentsationaal@gmail.com>
2024-01-11 11:54:48 +0300

    tls cleanup

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,7 +3,9 @@ const { sendEmail } = require('../essential funcs/generateEmail/sendMail');
 const router = require('express').Router()
 
 router.get('/scentsational/sendEmail/', (req, res) => {
-    sendEmail(req.query)
+    sendEmail(req.query).then((e) => {
+        res.send(e)
+    })
 })
 
 module.exports = router;
